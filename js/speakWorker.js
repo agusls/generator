60a5bfea8726fda54db3c65370d05119e5862bb6
agusls <wilgautama@gmail.com>
2021-06-09 02:06:55 +0700

    Update speakWorker.js

--- js/speakWorker.js
+++ js/speakWorker.js
@@ -1,4 +1,4 @@
-importScripts('speakGenerator.js');
+importScripts('https://agusls.github.io/generator/js/speakGenerator.js');
 onmessage = function (event) {
     postMessage(generateSpeech(event.data.text, event.data.args));
 };
