3e69f2c4ca36c27c43be50625a7e007c15c3c290
agusls <wilgautama@gmail.com>
2021-07-28 12:08:18 +0700

    2

--- Random-Phone/js/phone.js
+++ Random-Phone/js/phone.js
@@ -4,27 +4,6 @@ var modal = document.getElementById("countryModal");
 // Get the button that opens the modal
 var btn = document.getElementById("country");
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
-
-// When the user clicks the button, open the modal 
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
-
-// When the user clicks anywhere outside of the modal, close it
-window.onclick = function(event) {
-  if (event.target == modal) {
-    modal.style.display = "none";
-  }
-}
-
-
 $(document).ready(function() {
 	
     getPhoneNumber();
@@ -79,7 +58,7 @@ $(document).ready(function() {
         console.log(selectedItemSrc);
         console.log(selectedItemCode);
 
-        modal.style.display = "none";
+        $.modal.close();
     });
 
 
