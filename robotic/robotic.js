9aba2c0d632d6bdeb65451843fabee3715154a7f
agusls <wilgautama@gmail.com>
2022-07-25 17:05:46 +0700

    Update robotic.js

--- robotic/robotic.js
+++ robotic/robotic.js
@@ -30,3 +30,45 @@ script.addEventListener('load', function () {
         });
     });
 }, false);
+
+
+$("#input-text").on('keypress', function () {
+            $("#download_link").css('opacity', 0.1);
+            $("#download_link").css('pointer-events', 'none');
+        })
+
+        var play_button = $(".result").append(
+            "<div id='button_container'></div>"
+        );
+
+        var voice_speed = 158;
+        $("#button_container").append(
+            '<div class="setting">' +
+            '    <div>Speed</div>' +
+            '    <div>' +
+            '        <input class="slider" id="voice_speed" type="range" min="5" max="300" value="158" step="1">' +
+            '    </div>' +
+            '</div>'
+        )
+        $("#voice_speed").on("input", function () {
+            voice_speed = this.value;
+        });
+        var voice_pitch = 20;
+        $("#button_container").append(
+            '<div class="setting">' +
+            '    <div>Pitch</div>' +
+            '    <div>' +
+            '        <input class="slider" id="voice_pitch" type="range" min="0.1" max="100" value="20" step="0.1">' +
+            '    </div>' +
+            '</div>'
+        )
+        $("#voice_pitch").on("input", function () {
+            voice_pitch = this.value;
+        });
+
+        $("#button_container").append(
+            '<div class="btn-voice"><button id="play" class="play">LOADING...</button>' +
+            '<a class="download" download="VoiceOfText-' + Date.now() + '" id="download_link">' +
+            '<i class="fa fa-download" aria-hidden="true"></i> Download Audio File' +
+            '</a></div>'
+        );
