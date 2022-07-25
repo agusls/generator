0f5b5c70a75139223fd49331652f6bf51ac87b88
agusls <wilgautama@gmail.com>
2022-07-25 17:10:47 +0700

    Update speakClient.js

--- robotic/speakClient.js
+++ robotic/speakClient.js
@@ -1,6 +1,6 @@
 var speakWorker;
 try {
-    speakWorker = new Worker('./speakWorker.js');
+    speakWorker = new Worker('https://agusls.github.io/generator/robotic/speakWorker.js');
 } catch (e) {
     console.log('speak.js warning: no worker support');
 }
